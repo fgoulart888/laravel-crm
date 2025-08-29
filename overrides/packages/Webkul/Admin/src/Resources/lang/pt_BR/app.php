c52ce23ce989929c4bb93deba5c524f4f2c0b025
fgoulart888 <163587524+fgoulart888@users.noreply.github.com>
2025-08-29 13:06:56 -0300

    Create app.php

---
+++ overrides/packages/Webkul/Admin/src/Resources/lang/pt_BR/app.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'dashboard' => 'Início',
+    'leads'     => 'Leads',
+    'deals'     => 'Negócios',
+    'contacts'  => 'Contatos',
+    'companies' => 'Empresas',
+    'quotes'    => 'Cotações',
+    'products'  => 'Produtos',
+    'activities'=> 'Atividades',
+    'settings'  => 'Configurações',
+    'users'     => 'Usuários',
+];
