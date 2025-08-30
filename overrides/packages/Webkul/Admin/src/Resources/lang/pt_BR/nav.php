8eae742aad5dcd2218b923a0fe20dc751c2622e5
fgoulart888 <163587524+fgoulart888@users.noreply.github.com>
2025-08-30 11:34:00 -0300

    Create nav.php

---
+++ overrides/packages/Webkul/Admin/src/Resources/lang/pt_BR/nav.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'dashboard'     => 'Início',
+    'leads'         => 'Leads',
+    'quotes'        => 'Propostas',
+    'mail'          => 'E-mails',
+    'activities'    => 'Atividades',
+    'contacts'      => 'Contatos',
+    'products'      => 'Produtos',
+    'settings'      => 'Configurações',
+    'configuration' => 'Configuração',
+];
