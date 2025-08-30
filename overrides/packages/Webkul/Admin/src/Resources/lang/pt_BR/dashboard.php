749ceb9d0fbcdc136d1882e4fd6d95e65a759d01
fgoulart888 <163587524+fgoulart888@users.noreply.github.com>
2025-08-30 11:35:01 -0300

    Create dashboard.php

---
+++ overrides/packages/Webkul/Admin/src/Resources/lang/pt_BR/dashboard.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'title'                 => 'Início',
+    'won-deals'             => 'Negócios Ganhos',
+    'lost-deals'            => 'Negócios Perdidos',
+    'avg-ticket'            => 'Ticket médio',
+    'total-deals'           => 'Total de negócios',
+    'avg-deals-per-day'     => 'Média de negócios por dia',
+    'total-quotes'          => 'Total de cotações',
+    'total-persons'         => 'Total de pessoas',
+    'total-organizations'   => 'Total de empresas',
+    'deals'                 => 'Negócios',
+    'deals-by-stage'        => 'Negócios por estágios',
+    'revenue-by-source'     => 'Faturamento por origens',
+    'revenue-by-types'      => 'Faturamento por tipos',
+    'top-products'          => 'Produtos em destaque',
+    'top-persons'           => 'Pessoas em destaque',
+    'no-data'               => 'Nenhum dado disponível para o intervalo selecionado',
+];
